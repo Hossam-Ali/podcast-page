cdc4885b0c447097d7649f12c96b7c47f3e3176b
Hossam Ali <hossamm.ali1993@gmail.com>
2024-01-04 02:50:19 +0100

    test

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -9,22 +9,15 @@ interface dropdownProps {
 
 export const Dropdown = ({ items }: dropdownProps) => (
   <div className="dropdown top-0 align-middle">
-    <details className="dropdown-details flex align-middle">
-      <summary className="dropdown-dots m-1 btn cursor-pointer bg-transparent">
-        <Image src={DropdownDots} alt="dropdown dots" />
-      </summary>
-      <ul className="p-2 shadow menu dropdown-content absolute z-[1] bg-base-100 rounded-box w-52">
-        {items.map((val: string, ind: number) => {
-          if (val === 'divider')
-            return <div className="divider block" key={ind} />;
-          return (
-            <li key={ind}>
-              <a className="w-ful text-sm cursor-pointer text-left block truncate py-2 px-3">
-                {val}
-              </a>
-            </li>
-          );
-        })}
+    <details className="dropdown">
+      <summary className="m-1 btn">:</summary>
+      <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+        <li>
+          <a>Item 1</a>
+        </li>
+        <li>
+          <a>Item 2</a>
+        </li>
       </ul>
     </details>
   </div>
